bfc0958964473482d2ea80395d4431c29e1a7760
devusmonjon <usmonjonhasanov777@gmail.com>
2024-09-13 10:27:00 +0500

    fix

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <nav className="sticky top-0">
-      <div className="container">
+    <nav
+      className={`sticky top-0 z-[100] py-[24px] duration-300 flex items-center ${
+        scrollY > 38 ? "backdrop-blur-xl py-[14px]" : "bg-transparent"
+      }`}
+    >
+      <div className="container flex items-center">
         <Link to={"/"} className="text-[32px] font-bold font-integral">
           SHOP.CO
         </Link>
